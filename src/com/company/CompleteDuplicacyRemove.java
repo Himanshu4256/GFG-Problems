a5cfc3cb805670ef8b1030968bd4d2f13372684e
Himanshu4256 <mhimanshu2104@gmail.com>
2023-08-06 22:44:28 +0530

    initial commit

---
+++ src/com/company/CompleteDuplicacyRemove.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompleteDuplicacyRemove {
+    public static void main(String[] args) {
+        int arr[] = {2,3,5,7,4,3,4};
+        Map<Integer,Integer> m = new HashMap<>();
+        for (Integer s :arr) {
+            if (m.containsKey(s)){
+                m.put(s,m.get(s)+1);
+            }
+            else {
+                m.put(s, 1);
+            }
+        }
+        for (Integer key: m.keySet()) {
+            if (m.get(key)==1){
+                System.out.println(key+" ");
+            }
+        }
+    }
+}
